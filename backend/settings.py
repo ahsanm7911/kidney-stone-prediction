4ed59a500645aa4b5122d9c24e8ab9fcdb3320e5
muhammad ahsan <ahsanm7911@gmail.com>
2025-01-20 16:33:13 +0500

    basic template setup

--- backend/settings.py
+++ backend/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assets/'
+STATIC_ROOT = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assets'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
